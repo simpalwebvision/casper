cb459aabc13e1601972d172cd237b800edbcd96e
HydronSid <suvisiddhant6101999@gmail.com>
2024-11-16 15:35:17 +0530

    added technician list

--- lib/Screens/Type-SuperAdmin/Employees/Employee-List/employee_list_controller.dart
+++ lib/Screens/Type-SuperAdmin/Employees/Employee-List/employee_list_controller.dart
@@ -1,10 +1,56 @@
+import 'package:caspro_enterprises/Models/user_profile_model.dart';
+import 'package:caspro_enterprises/Repository/auth_repository.dart';
+import 'package:caspro_enterprises/Utils/common_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class EmployeeListController extends GetxController {
+class EmployeeListController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  AuthRepository authRepository = AuthRepository();
+  late TabController tabController;
   Rx<TextEditingController> ctlSearchController = TextEditingController().obs;
 
-  Future fetchData() async {}
+  RxBool isSelectTechincian = false.obs;
+
+  RxBool isTechnicianLoading = false.obs;
+  techListLoadingFun(bool val) => isTechnicianLoading.value = val;
+
+  RxList<TechnicianProfileModel> technicianList =
+      <TechnicianProfileModel>[].obs;
+
+  @override
+  void onInit() {
+    tabController = TabController(length: 2, vsync: this);
+
+    initData();
+    super.onInit();
+  }
+
+  initData() async {
+    await Future.wait([getTechnicianList()]);
+  }
+
+  Future getTechnicianList() async {
+    techListLoadingFun(true);
+    var response = await authRepository.getTechincianList();
+
+    response.fold((error) {
+      CommonFunctions.showGetxSnackBar("Error", msg: error.message);
+      techListLoadingFun(false);
+    }, (data) {
+      technicianList.value = data;
+      techListLoadingFun(false);
+    });
+  }
+
+  onChangeTab(value) {
+    tabController.index = value;
+    if (value == 0) {
+      isSelectTechincian.value = false;
+    } else {
+      isSelectTechincian.value = true;
+    }
+  }
 
   @override
   void dispose() {
--- lib/Screens/Type-SuperAdmin/Employees/Employee-List/employees_list.dart
+++ lib/Screens/Type-SuperAdmin/Employees/Employee-List/employees_list.dart
@@ -1,8 +1,11 @@
 import 'package:caspro_enterprises/Utils/app_constants.dart';
+import 'package:caspro_enterprises/Utils/app_images.dart';
 import 'package:caspro_enterprises/Utils/common_appbar.dart';
+import 'package:caspro_enterprises/Widgets/common_loader.dart';
 import 'package:caspro_enterprises/Widgets/custom_image.dart';
 import 'package:caspro_enterprises/Widgets/detail_widget_helper.dart';
 import 'package:caspro_enterprises/Widgets/input_fields.dart';
+import 'package:caspro_enterprises/Widgets/no_data_found.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -41,105 +44,155 @@ class _EmployeesListState extends State<EmployeesList> {
               // Handle change
             },
           ),
-          Expanded(
-            child: RefreshIndicator(
-              onRefresh: () => controller.fetchData(),
-              child: ListView.separated(
-                physics: const BouncingScrollPhysics(),
-                padding: const EdgeInsets.only(bottom: 15),
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 10,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TabBar(
+              unselectedLabelColor: secondaryColor,
+              padding: const EdgeInsets.only(bottom: 5),
+              indicatorColor: primaryColor,
+              controller: controller.tabController,
+              labelColor: primaryColor,
+              labelStyle: GoogleFonts.mukta(fontWeight: FontWeight.bold),
+              onTap: (value) => controller.onChangeTab(value),
+              tabs: const [
+                Tab(
+                  text: 'Office',
                 ),
-                itemCount: 6,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {},
-                    child: Card(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      shadowColor: blackColor,
-                      clipBehavior: Clip.hardEdge,
-                      shape: RoundedRectangleBorder(
-                          side: const BorderSide(color: borderColor),
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const ProfileCustomImage(
-                              image:
-                                  "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg",
-                              errorImage:
-                                  "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg",
-                              imgHeight: 50,
-                              imgWidth: 50,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  DetailWidgetHelper(
-                                    heading: "User name",
-                                    value: "Siddhant mestry",
-                                    valueColor: blueColor,
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  DetailWidgetHelper(
-                                    heading: "Mobile no.",
-                                    value: "Inv-150",
-                                    valueColor: blueColor,
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  const DetailWidgetHelper(
-                                    heading: "Address",
-                                    value: "",
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  const Divider(
-                                    color: blackColor,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Technician",
-                                        style: GoogleFonts.poppins(
-                                            fontSize: 13,
-                                            color: primaryColor,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+                Tab(
+                  text: 'Technician',
+                ),
+              ],
             ),
           ),
+          Obx(
+            () => controller.isSelectTechincian.value
+                ? Expanded(
+                    child: RefreshIndicator(
+                      onRefresh: () => controller.getTechnicianList(),
+                      child: controller.isTechnicianLoading.value
+                          ? const CommonLoader()
+                          : controller.technicianList.isEmpty
+                              ? const NoDataFoundScreen(
+                                  image: AppImages.emptyData,
+                                  passedData: "No Technician Found ..",
+                                )
+                              : ListView.separated(
+                                  physics: const BouncingScrollPhysics(),
+                                  padding: const EdgeInsets.only(bottom: 15),
+                                  separatorBuilder: (context, index) =>
+                                      const SizedBox(
+                                    height: 10,
+                                  ),
+                                  itemCount: controller.technicianList.length,
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, index) {
+                                    var technician =
+                                        controller.technicianList[index];
+                                    return InkWell(
+                                      onTap: () {},
+                                      child: Card(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 8),
+                                        shadowColor: blackColor,
+                                        clipBehavior: Clip.hardEdge,
+                                        shape: RoundedRectangleBorder(
+                                            side: const BorderSide(
+                                                color: borderColor),
+                                            borderRadius:
+                                                BorderRadius.circular(12)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              ProfileCustomImage(
+                                                image: technician.image ?? "",
+                                                errorImage:
+                                                    "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg",
+                                                imgHeight: 50,
+                                                imgWidth: 50,
+                                              ),
+                                              const SizedBox(
+                                                width: 10,
+                                              ),
+                                              Expanded(
+                                                child: Column(
+                                                  children: [
+                                                    const SizedBox(
+                                                      height: 5,
+                                                    ),
+                                                    DetailWidgetHelper(
+                                                      heading: "User name",
+                                                      value:
+                                                          technician.name ?? "",
+                                                      valueColor: blueColor,
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 5,
+                                                    ),
+                                                    DetailWidgetHelper(
+                                                      heading: "Mobile no.",
+                                                      value:
+                                                          technician.mobile ??
+                                                              "",
+                                                      valueColor: blueColor,
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 5,
+                                                    ),
+                                                    DetailWidgetHelper(
+                                                      heading: "Address",
+                                                      value:
+                                                          technician.address ??
+                                                              "",
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 5,
+                                                    ),
+                                                    const Divider(
+                                                      color: blackColor,
+                                                    ),
+                                                    Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .start,
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .center,
+                                                      children: [
+                                                        Text(
+                                                          "Technician",
+                                                          style: GoogleFonts
+                                                              .poppins(
+                                                                  fontSize: 13,
+                                                                  color:
+                                                                      primaryColor,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                    ),
+                  )
+                : const SizedBox(),
+          )
         ],
       ),
     );
